ffcc8ead67f9a38d8ed59cd62d1922373c7134cd
drex007 <vykeoj@gmail.com>
2023-04-29 10:55:51 +0000

    contract inherit Ownable from openzepplin

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -2,23 +2,23 @@
 // 1. Pragma
 // pragma solidity ^0.8.8;
 
-pragma solidity ^0.8.0;
+pragma solidity >=0.6.0;
+import 'openzeppelin-solidity/contracts/access/Ownable.sol';
 
-
-contract Greeter {
+contract Greeter is Ownable{ // Allow our greeter contract to inherit from OWnable contract from openzeeplin
      string public _greeting = "Hello world";
      address private _owner;
      constructor ()  {
         _owner = msg.sender;
      }
-    modifier onlyOwner(){
-        require(
-            msg.sender == _owner,
-            "Ownable: caller is not the owner"
-        );_;
-    }
+    // modifier onlyOwner(){
+    //     require(
+    //         msg.sender == _owner,
+    //         "Ownable: caller is not the owner"
+    //     );_;
+    // }
 
-function setGreeting(string calldata greeting) external onlyOwner{ // added a modifier to be called befpre
+function setGreeting(string calldata greeting) external { // added a modifier to be called befpre
 _greeting = greeting;
 
 }
@@ -26,7 +26,7 @@ _greeting = greeting;
 function greet() external view returns (string memory){
     return _greeting;
 }
-function owner() public view returns(address){
-    return _owner;
-}
+// function owner() public view returns(address){
+//     return _owner;
+// }
 }
